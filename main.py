bdf30b0d8914ce5815ae3d02820c3c1df0fd9686
Tung Pham <joseph.pham280996@gmail.com>
2023-11-21 08:53:28 -0500

    Add some basic files

---
+++ main.py
@@ -0,0 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--data", default="data", help="The path to boxes JSON file for testing")
+parser.add_argument("-f", "--format", default=1, type=int, help="The format of the input file (1: JSON, 2: CSV)")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    file_data = getattr(args, 'data')
+    file_format = getattr(args, 'format')
